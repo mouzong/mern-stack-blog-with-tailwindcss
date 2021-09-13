09990e8285f7f69ccb83b67b8a029ca2beca27c5
mouzong <andreasmouzong@gmail.com>
2021-09-13 06:11:14 +0100

    Added routing to our app and created the home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="container bg-blue-500 p-5 text-center">
-      Halowa
-    </div>
+    <Router>
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
   );
 }
 

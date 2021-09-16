88b0b65f50c1061f2f8ef0525d991e18d3b9c78a
mouzong <andreasmouzong@gmail.com>
2021-09-16 09:52:45 +0100

    finished the articles list page

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,11 +1,28 @@
 import React from "react";
+import articleContent from "./article-content";
 
-const Article = () => {
+const Article = ({ match }) => {
+  const name = match.params.name;
+  const article = articleContent.find((article) => article.name === name);
+  if (!article)
+    return (
+      <h1>
+        Article <span className=""> {name}</span> does not exist
+      </h1>
+    );
   return (
     <>
       <h1 className="sm:text-4xl text-2xl font-bold mb-6 mt-6 text-gray-900">
-        This is an Article
+        {article.title}
       </h1>
+      {article.content.map((paragraph, index) => (
+        <p
+          className="mx-auto leading-relaxed text-base mb-4 text-justify"
+          key={index}
+        >
+          {paragraph}
+        </p>
+      ))}
     </>
   );
 };
--- src/pages/ArticleList.js
+++ src/pages/ArticleList.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import articles from "./article-content";
 
 const ArticleList = () => {
   return (
@@ -6,6 +8,31 @@ const ArticleList = () => {
       <h1 className="sm:text-4xl text-2xl font-bold mb-6 mt-6 text-gray-900">
         Articles
       </h1>
+      <div className="container py-4 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          {articles.map((article, index) => (
+            <div key={index} className="p-4 md:w=full">
+              <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                <Link to={`/article/${article.name}`}>
+                  <img
+                    src={article.thumbnail}
+                    className="lg:h-48 md:h-36 w-full object-cover object-center"
+                    alt="blog"
+                  />
+                </Link>
+                <div className="p-6">
+                  <Link key={index} to={`/article/${article.name}`}>
+                    <h3 className="title-font text-lg font-medium text-gray-900 mb-3">
+                      {article.title}
+                    </h3>
+                  </Link>
+                  <p>{article.content[0].substring(0, 120)}...</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
